c48e6d2897fc048aa1f3bb6162dd2981cd8ebf39
mcincura <mar.cincura@gmail.com>
2025-07-20 16:51:37 +0200

    changes to deploy on gh pages

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,19 +1,24 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
 import Landing from '../pages/landing/landing';
 import Dash from '../pages/dashboard/dashboard';
 import Payment from '../components/payment/payment';
 
 const App = () => {
-    return (
-        <div className="App">
-            <Router>
-                <Routes>
-                    <Route path='/' element={<Landing />} />
-                    <Route path='/test' element={<Payment />} />
-                </Routes>
-            </Router>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/test" element={<Payment />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
-export default App
+export default App;
