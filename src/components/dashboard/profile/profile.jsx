9b8e539adcfffb8529e6f63b27e00fccbe3f8d2e
mcincura <mar.cincura@gmail.com>
2025-08-18 20:32:00 +0200

    balatro koktiny

--- src/components/dashboard/profile/profile.jsx
+++ src/components/dashboard/profile/profile.jsx
@@ -1,9 +1,14 @@
 import './profile.css'
+import Balatro from '../../ui/balatro/balatro';
 
 const Profile = () => {
     return (
         <div className="profile-main">
-            <h1>ABY SI ZDECHEL PROFILE</h1>
+            <Balatro
+                isRotate={false}
+                mouseInteraction={false}
+                pixelFilter={2000}
+            />
         </div>
     )
 }
